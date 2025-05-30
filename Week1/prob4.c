7af46a94961e7613efccd80bf32ee2f0d6b205e4
Hamzah <hamzahahmed324@gmail.com>
2025-05-30 14:31:24 +0900

    Add files via upload

---
+++ Week1/prob4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+float fahr, celsius;
+int lower, upper, step;
+lower = 0;
+upper = 300;
+step = 20;
+fahr=lower;
+while (fahr <= upper) {
+celsius = (fahr - 32) * 5 / 9;
+printf("%6.1f %6.1f\n",fahr,celsius);
+fahr = fahr+step;
+}
+return 0;
+}
---
+++ Week1/problem2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+printf("%10d\n",42);
+return 0;
+}
